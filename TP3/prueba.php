2bee1e9ea7bcb808f1c66f76b4ff2da35984912f
Veronica Arce <veronica.arce@est.fi.uncoma.edu.ar>
2022-08-23 23:24:25 -0300

    Primer commit

---
+++ TP3/prueba.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Vista/Bootstrap/css/bootstrap.min.css">
+    <script src="Vista/Bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Agregar">
+        </div>
+    </form>
+</body>
+</html>
