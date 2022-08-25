56c90d56c8cbd1ad530d710c803b9e1e81a731d3
Veronica Arce <veronica.arce@est.fi.uncoma.edu.ar>
2022-08-24 21:34:39 -0300

    segundo commit

---
+++ TP1/Control/ej2cantidadhoras.php
@@ -0,0 +1,37 @@
+<?php
+if ($_GET){
+    $lunes=$_GET['inglunes'];
+    $martes=$_GET['ingmartes'];
+    $miercoles=$_GET['ingmiercoles'];
+    $jueves=$_GET['ingjueves'];
+    $viernes=$_GET['ingviernes'];
+
+    $horario = [];
+    $horario[0] =[
+        "día" => "lunes",
+        "horas" => $lunes,
+    ];
+    $horario[1] =[
+        "día" => "martes",
+        "horas" => $martes,
+    ];
+    $horario[2] =[
+        "día" => "miercoles",
+        "horas" => $miercoles,
+    ];
+    $horario[3] =[
+        "día" => "jueves",
+        "horas" => $jueves,
+    ];
+    $horario[4] =[
+        "día" => "viernes",
+        "horas" => $viernes,
+    ];
+
+$sumahoras = array($lunes, $martes, $miercoles, $jueves, $viernes);
+echo "Cantidad de horas Prog. Dinamica =" . array_sum($sumahoras). "</br>";
+ 
+}
+
+?>
+<a href="ej2.php">Volver a inicio</a>
---
+++ TP1/Vista/ej2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio2_Tp1</title>
+</head>
+<body>
+<form id="horacursada" name="horacursada" method="GET" action="../Control/ej2cantidadhoras.php">
+    <h3>Materia Programacion Web Dinamica</h3></br>
+    Ingresar las horas de cursada: </br>
+    Lunes <input name="inglunes" type="text" id="inglunes"/></br>
+    Martes <input name="ingmartes" type="text" id="ingmartes"/></br>
+    Miercoles <input name="ingmiercoles" type="text" id="ingmiercoles"/></br>
+    Jueves <input name="ingjueves" type="text" id="ingjueves"/></br>
+    Viernes <input name="ingviernes" type="text" id="ingviernes"/></br>
+    <input type="submit" name="submit" value="Aceptar"/>
+</body>
+</html>
